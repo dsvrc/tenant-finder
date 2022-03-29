2fe6526516a55cf570da21dab49a510416f54e0b
dsvrc <101639707+dsvrc@users.noreply.github.com>
2022-03-29 12:04:29 +0530

    mid eval push

--- app/src/main/java/com/example/tenantfinder_new/MainActivity.java
+++ app/src/main/java/com/example/tenantfinder_new/MainActivity.java
@@ -16,7 +16,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
     EditText username,password;
     Button login;
     TextView newuser;
