64b50251dd25894ed90d309d73efb66628b08f6a
dsvrc <101639707+dsvrc@users.noreply.github.com>
2022-04-14 23:05:15 +0530

    ownerside changes

--- app/src/main/java/com/example/tenantfinder_new/OwnerActivity.java
+++ app/src/main/java/com/example/tenantfinder_new/OwnerActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -16,7 +17,7 @@ Button addnewproperty;
 List<List<String>> propertiesdata;
     RecyclerView rvContacts;
     ImageView userprofile;
-
+    TextView welcomeuser;
 void method2(List<List<String>> lists)
 {
     TenantpropertieshomeActivity adapterobj=new TenantpropertieshomeActivity(lists,"owner");
@@ -28,28 +29,34 @@ void method2(List<List<String>> lists)
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner);
         addnewproperty=(Button) findViewById(R.id.addnewproperty);
+        Intent homeintent = getIntent();
+        String usernamee = homeintent.getStringExtra("username");
+        welcomeuser=(TextView) findViewById(R.id.welcomeuser);
         userprofile=(ImageView) findViewById(R.id.userprofile);
-        Intent ownerintent = getIntent();
-        String usernamee = ownerintent.getStringExtra("username");
+
         userprofile.setOnClickListener((view2)->{
             System.out.println("boo");
             Intent userprofileintent=new Intent(OwnerActivity.this,UserprofileActivity.class);
             userprofileintent.putExtra("username",usernamee);
             startActivity(userprofileintent);
         });
-
+        welcomeuser.setText("Welcome "+usernamee);
         addnewproperty.setOnClickListener((view)->
         {
+
             Intent addproperty=new Intent(OwnerActivity.this,AddNewPropertyActivity.class);
+            addproperty.putExtra("username",usernamee);
+
             startActivity(addproperty);
         });
          rvContacts = (RecyclerView) findViewById(R.id.rvownerproperties);
-        PropertiesDataActivity.ownerside("venkat", new Returnpropertiesdata() {
+
+        PropertiesDataActivity.ownerside(usernamee, new Returnpropertiesdata() {
             @Override
             public  void onSuccess(List<List<String>> lists2){
                 propertiesdata=lists2;
                 System.out.println(propertiesdata);
-       method2(propertiesdata);
+                method2(propertiesdata);
             }
 
         });
